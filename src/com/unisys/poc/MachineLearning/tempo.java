a9acd22b3137d545dcdef1e2bb2eee7a1be4980e
unknown <chaitimekr@gmail.com>
2015-08-02 15:17:59 +0530

    Maven updates

---
+++ src/com/unisys/poc/MachineLearning/tempo.java
@@ -0,0 +1,12 @@
+package com.unisys.poc.MachineLearning;
+
+public class tempo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WekaProcess proc  = new WekaProcess();
+		proc.arffToDatabase("D:\\Weka\\weka-service-master\\lib\\api-weather");
+        
+	}
+
+}
